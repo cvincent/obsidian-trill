51ce232fe4ccf457c66ff5658919420f5c446994
Chris Vincent <chris.vincent@hey.com>
2025-03-17 13:28:14 -0500

    Buildable

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import { Plugin } from 'obsidian';
-import { main, set_checkbox_at_path_line } from "./build/dev/javascript/obsidian_plugin/main.mjs";
+import { main } from "./build/dev/javascript/obsidian_plugin/main.mjs";
 
 export default class MyPlugin extends Plugin {
   async onload() {
