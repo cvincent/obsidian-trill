d700cca2577e2d28527f44fc06de359f64b79fcc
Chris Vincent <chris.vincent@hey.com>
2025-04-26 21:42:22 -0500

    Formatting

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
-import { Plugin } from 'obsidian';
+import { Plugin } from "obsidian";
 import { main } from "./build/dev/javascript/obsidian_plugin/main.mjs";
 
 export default class Trill extends Plugin {
   async onload() {
-    main(this)
+    main(this);
   }
 
   onunload() {
